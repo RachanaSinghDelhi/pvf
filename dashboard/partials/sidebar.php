e8e40c5255e2269727beecc05c416df14042e0d4
Nice Web Technologies <nicewebtechnologies@gmail.com>
2024-10-27 18:06:17 +0530

    commit

--- dashboard/partials/sidebar.php
+++ dashboard/partials/sidebar.php
@@ -3,12 +3,12 @@
         <nav class="sb-sidenav accordion sb-sidenav-dark" id="sidenavAccordion">
             <div class="sb-sidenav-menu">
                 <div class="nav">
-                    <!-- Core Section
+                    <!-- Core Section-->
                     <div class="sb-sidenav-menu-heading">Core</div>
-                    <a class="nav-link" href="/dash">
+                    <a class="nav-link" href="index.php">
                         <div class="sb-nav-link-icon"><i class="fas fa-tachometer-alt"></i></div>
                         Dashboard
-                    </a> -->
+                    </a> 
 
                     <!-- Members Section -->
                     <div class="sb-sidenav-menu-heading">Members</div>
